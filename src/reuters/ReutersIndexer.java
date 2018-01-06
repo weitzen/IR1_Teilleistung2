d3775168ae70c825912a5c25603e9d0a8e91da08
Simon <Simon.Weitz@web.de>
2018-01-06 18:23:00 +0100

    added 7.1 bitte 'index' Ordner im gleich Verzeichnis wie 'src' oder 'lib' anlegen, damit es sicher funktioniert

--- src/reuters/ReutersIndexer.java
+++ src/reuters/ReutersIndexer.java
@@ -1,16 +1,24 @@
 package reuters;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.BinaryDocValuesField;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.NumericDocValuesField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+
 
 /**
  * Klasse zum Indexieren der Texte
@@ -26,9 +34,13 @@ public class ReutersIndexer {
 	public static void main(String[] args) throws Exception {
 
 		// TODO: passen Sie die Pfade ggf. an!
-		String filePath = new File("").getAbsolutePath();
+		//String filePath = new File("").getAbsolutePath();
+		
+		/**
+		 * fileName = contentDir
+		 */
 		String fileName = "data/2000reuters";
-		String indexDir = "idx";
+		String indexDir = "index";
 
 		ReutersIndexer indexer = new ReutersIndexer(indexDir);
 		
@@ -65,8 +77,31 @@ public class ReutersIndexer {
 	}
 
 	protected int index(File[] files) throws Exception {
-		// TODO Hier bitte implementieren
+		
+		for (File f : files) {
+			if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead()) {
+				indexFile(f);
+			}
+		}
+
+		return writer.numDocs(); 
+	}
 	
-		return -1;
+	protected Document getDocument(File f) throws Exception {
+		Document doc = new Document();
+		// XXX 7: neu: TextField, um Dateiinhalt zu indexieren
+		doc.add(new TextField(CONTENT, new FileReader(f))); // 7
+		// XXX 8: neu: numerische Feld, um Filegröße zu indexieren
+		doc.add(new NumericDocValuesField(FILESIZE, f.length()));// 8
+		// XXX 9: neu: StringField, um Dateipfad zu indexieren
+		doc.add(new StringField(PATH, f.getCanonicalPath(), // 9
+				Field.Store.YES));// 9
+		return doc;
+	}
+	
+	private void indexFile(File f) throws Exception {
+		System.out.println("Indexing " + f.getCanonicalPath());
+		Document doc = getDocument(f);
+		writer.addDocument(doc); // 10
 	}
 }
